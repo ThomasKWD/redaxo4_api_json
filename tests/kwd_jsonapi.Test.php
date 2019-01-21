e6f7b59eab85ab57bcd6242a3eeb8dd173aea58b
Thomas Kühne <thomas@kuehne-webdienste.de>
2019-01-21 22:01:50 +0100

    tests with OOCatagory mock object (draft)

--- tests/kwd_jsonapi.Test.php
+++ tests/kwd_jsonapi.Test.php
@@ -3,11 +3,37 @@ use PHPUnit\Framework\TestCase;
 
 require_once('../classes/kwd_jsonapi.php');
 
+class mockRexCategory {
+
+	private $id;
+	private $name;
+
+	function __construct($id,$name) {
+		$this->id = $id;
+		$this->name = $name;
+	}
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function getName() {
+		return $this->name;
+	}
+}
+
 // need an extra derived class
 class kwd_jsonapi_test extends kwd_jsonapi {
 
 	public function getRootCategories($ignore_offlines = false, $clang = 0) {
-		return null;
+		// to mock the root categories we just generate objects from a json
+		$rootCats = array();
+		$rootCats[] = new mockRexCategory(1,'Start');
+		// array_push(new mockRexCategory(21,'News'));
+		// array_push(new mockRexCategory(4,'Angebote'));
+		// array_push(new mockRexCategory(3,'Referenzen'));
+
+		return $rootCats;
 	}
 
 	public function getCategoryById($id, $clang = 0) {
@@ -22,7 +48,7 @@ class kwd_jsonapi_test extends kwd_jsonapi {
 		parent::__construct(
 			'GET',
 			'http',
-			'api=articles/4',
+			'api=',
 			'localhost/tk/kwd_website//'
 		);
 	}
@@ -45,7 +71,7 @@ class KwdJsonApiTestCase extends TestCase {
 		$this->assertArrayHasKey('requestMethod',$conf); // redundant because array indices used below
 		$this->assertSame($conf['requestMethod'],'get','request method must be valid case insensitive');
 		$this->assertSame($conf['baseUrl'],'http://localhost/tk/kwd_website/api/','baseUrl must eliminate >1 trailing slashes, but add if none');
-		$this->assertSame($conf['apiString'],'api=articles/4','queryString must have "api="...');
+		$this->assertSame($conf['apiString'],'api=','queryString must have "api="...');
     }
 
 	public function testReInit() {
@@ -92,13 +118,6 @@ class KwdJsonApiTestCase extends TestCase {
 		$this->assertEquals($this->jsonApiObject->getHeaders()[1],$accesControlOrigin,'sample check index 1');
 	}
 
-	public function testIgnoreApiOnBuildResponse() {
-		$this->jsonApiObject->init('GET','http','article_id=23','localhost');
-		$ret = $this->jsonApiObject->buildResponse(); // better name
-		$this->assertTrue(is_string($ret));
-		$this->assertSame($ret,'','no API request must lead to empty string');
-	}
-
 	public function testApiRootResponse() {
 		$this->jsonApiObject->init('GET','http','api=','localhost');
 		$ret = $this->jsonApiObject->buildResponse(); // better name
@@ -107,6 +126,36 @@ class KwdJsonApiTestCase extends TestCase {
 		$retJSON = json_decode($ret); // makes object!
 		// $this->assertJsonStringEqualsJsonString($ret,'API request must lead to sensible json string');
 		$this->assertSame($retJSON->request,'api/','checking json entry');
+	}
+
+	public function testIgnoreApiOnBuildResponse() {
+		$this->jsonApiObject->init('GET','http','article_id=23','localhost');
+		$ret = $this->jsonApiObject->buildResponse();
+		$this->assertTrue(is_string($ret));
+		$this->assertSame($ret,'','no API request must lead to empty string');
+	}
+
+	public function testGenerateResponseForEntryPoint() {
+		$this->markTestIncomplete('// ??? IDEA: could demand "/api/categories" while /api/just gives meta infos');
+	}
+
+	public function testGenerateResponseForRootRequest() {
+		$response = $this->jsonApiObject->buildResponse();
+		$this->assertInternalType('string',$response);
+		$this->assertGreaterThan(3,strlen($response)); // should not be empty (which is for rejected request)
+		// $this->assertEquals($response,'eeee'); // should not be empty (which is for rejected request)
+		// $response = '}(&%^)'.$response.'((**)*)';
+		$json = json_decode($response);
+		$this->assertNotEquals($json,null,'must be != null indicating correct JSON'); // should not be empty (which is for rejected request)
+		$this->assertEquals($json->help->info,'Check out the help section!','help text should be under help > info');
+		$this->assertTrue(is_array($json->help->links),'help links section should be array');
+		$this->assertEquals($json->help->links[0],'http://localhost/tk/kwd_website/api/help','help link should contain reasonable api link path');
+
+		// print_r($json);
+		$this->assertTrue(isset($json->root_articles));
+		$this->assertTrue(is_array($json->root_articles));
+		$this->assertEquals($json->root_articles[0]->title,'Start','Name of first root category');
+		// $this->assertTrue(isset($json->categories),'false means: the list of "categories" not found');
 	}
 
 	public function testSendResponse() {
